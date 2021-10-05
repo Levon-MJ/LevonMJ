55b3409502dd10e212147b62a2ad66e646136b93
Levon-MJ <Levon093612mail.ru>
2021-10-05 18:16:39 +0300

    changet ifExample, add Example

---
+++ src/lesson2/Example2.java
@@ -0,0 +1,20 @@
+package lesson2;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 5;
+        b = a * 5;
+        c = a + b;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+        if (a < b) {
+            System.out.println("a poqr e b-ic");
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("kyky" + i);
+        }
+
+    }
+}
---
+++ src/lesson2/Example3.java
@@ -0,0 +1,38 @@
+package lesson2;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 5;
+        b = a * 5;
+        c = a + b;
+        System.out.println("a=" + a);
+        System.out.println("b=" + b);
+        System.out.println("c=" + c);
+        if (a < b) {
+            System.out.println("a poqr e b-ic");
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("boom" + i);
+        }
+
+        int t = 112;
+        int y = 11;
+        if (t < y) {
+            System.out.println("ha");
+        } else {
+            System.out.println("che");
+        }
+
+        int g = 10;
+        System.out.println("g=" + g);
+    }
+
+
+}
+
+
+
+
+
+
